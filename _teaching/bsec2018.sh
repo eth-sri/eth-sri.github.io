f6e5be8b22e5164e850be758b159112d92009e1e
Petar Tsankov <ptsankov@inf.ethz.ch>
2018-09-20 20:34:25 +0200

    first assignment of bsec students

--- _teaching/bsec2018.sh
+++ _teaching/bsec2018.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat bsec2018.txt| awk -F '\t' '{print "<tr>\n\t<td>"$1"</td>\n\t<td><a href=\""$3"\" target=\"_blank\">"$2"</a></td>\n\t<td></td>\n\t<td></td>\n\t<td></td>\n</tr>"}'
+cat bsec2018.txt| awk -F '\t' '{print "<tr>\n\t<td>"$1"</td>\n\t<td><a href=\""$3"\" target=\"_blank\">"$2"</a></td>\n\t<td>"$4"</td>\n\t<td></td>\n\t<td><a href=\"mailto:"$6"\">"$5"</a></td>\n</tr>"}'
