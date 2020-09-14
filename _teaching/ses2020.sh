d33482c39441bbff1e9849611bc9cbfd4dc90898
Petar Tsankov <petar.tsankov@inf.ethz.ch>
2020-09-14 19:21:06 +0200

    ses papers

--- _teaching/ses2020.sh
+++ _teaching/ses2020.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat bsec2019.txt| awk -F '\t' '{print "<tr>\n\t<td>"$1"</td>\n\t<td>"$2"</td>\n\t<td><a href=\""$4"\" target=\"_blank\">"$3"</a></td>\n\t<td>"$5"</td>\n\t<td></td>\n\t<td><a href=\"mailto:"$7"\">"$6"</a></td>\n</tr>"}'
+cat ses2020.txt| awk -F '\t' '{print "<tr>\n\t<td>"$1"</td>\n\t<td><a href=\""$4"\" target=\"_blank\">"$3"</a></td>\n\t<td>"$5"</td>\n\t<td></td>\n\t<td><a href=\"mailto:"$7"\">"$6"</a></td>\n</tr>"}'
