731d7b471d446e922504fd08510824e3f1d10d77
Niels <niels.muendler@inf.ethz.ch>
2025-07-21 17:20:50 +0200

    Fix

--- _plugins/room_links.rb
+++ _plugins/room_links.rb
@@ -4,14 +4,12 @@
 Jekyll::Hooks.register :site, :post_render do |site|
   # Room regex pattern matching the Python script logic
   # Matches formats like: CAB G 56, CAB G56, HG F 81.1, HG F81.1
-  # Updated to handle cases where room mentions come after HTML tags
-  room_regex = /(?<space>\s+|\(\s*|,\s*|>)(?<building>[A-Z]+)\s(?<floor>[A-Z])\s?(?<room>\d+(?:\.\d+)?)/
+  room_regex = /\b(?<building>[A-Z]+)\s(?<floor>[A-Z])\s?(?<room>\d+(?:\.\d+)?)/
 
   site.pages.each do |page|
     if page.output_ext == '.html'
       original_content = page.output
       modified_content = original_content.gsub(room_regex) do |match|
-        space = $~[:space]
         building = $~[:building]
         floor = $~[:floor]
         room = $~[:room]
@@ -20,7 +18,7 @@ Jekyll::Hooks.register :site, :post_render do |site|
         link_url = "https://ethz.ch/en/utils/location.html?building=#{building}&floor=#{floor}&room=#{room}&lang=en"
         room_text = "#{building} #{floor} #{room}"
         
-        "#{space}<a href=\"#{link_url}\">#{room_text}</a>"
+        "<a href=\"#{link_url}\">#{room_text}</a>"
       end
       
       # Only update if content was modified
@@ -37,7 +35,6 @@ Jekyll::Hooks.register :site, :post_render do |site|
       if doc.output_ext == '.html'
         original_content = doc.output
         modified_content = original_content.gsub(room_regex) do |match|
-          space = $~[:space]
           building = $~[:building]
           floor = $~[:floor]
           room = $~[:room]
@@ -46,7 +43,7 @@ Jekyll::Hooks.register :site, :post_render do |site|
           link_url = "https://ethz.ch/en/utils/location.html?building=#{building}&floor=#{floor}&room=#{room}&lang=en"
           room_text = "#{building} #{floor} #{room}"
           
-          "#{space}<a href=\"#{link_url}\">#{room_text}</a>"
+          "<a href=\"#{link_url}\">#{room_text}</a>"
         end
         
         # Only update if content was modified
