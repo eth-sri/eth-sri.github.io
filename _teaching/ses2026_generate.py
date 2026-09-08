fa538b492ef6278612134582987b97bbcc671d32
Niels <niels.muendler@inf.ethz.ch>
2026-09-08 20:38:22 +0200

    Add papers for fall 2026 edition

--- _teaching/ses2026_generate.py
+++ _teaching/ses2026_generate.py
@@ -8,21 +8,21 @@ rows = []
 with open(filename, newline='') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
-        if any(row[0].startswith(s) for s in ("Lab", "By Lab", "Dates")) or not row[0]:
+        if row[0] not in ("AST", "SRI"):
             # skip headers
             continue
         rows.append(row)
 
 # Read the assignment file
 assignments = {}
-filename = sys.argv[2]
-with open(filename, newline='') as csvfile:
-    reader = csv.reader(csvfile)
-    for row in reader:
-        if any(row[0].startswith(s) for s in ("Name",)) or not row[0]:
-            # skip headers
-            continue
-        assignments[row[5].strip()] = row[0]
+# filename = sys.argv[2]
+# with open(filename, newline='') as csvfile:
+#     reader = csv.reader(csvfile)
+#     for row in reader:
+#         if any(row[0].startswith(s) for s in ("Name",)) or not row[0]:
+#             # skip headers
+#             continue
+#         assignments[row[5].strip()] = row[0]
 LAB, TITLE, VENUE, TA, PAPER_URL, DATE, TA_MAIL = range(7)
 
 # Parse dates and sort the entries by date
@@ -35,7 +35,9 @@ table_rows = []
 for i, row in enumerate(rows):
     date = row[DATE].strftime('%d %b') if i % 2 == 0 else ""
     paper_link = f'<a href="{row[PAPER_URL]}">{row[TITLE]}</a>' if row[PAPER_URL].strip().startswith("http") else row[TITLE]
-    ta = f'<a href="mailto:{row[TA_MAIL]}">{row[TA].split(" ")[0]}</a>'
+    ta = row[TA].split(" ")[0]
+    if row[TA_MAIL]:
+        ta = f'<a href="mailto:{row[TA_MAIL]}">{ta}</a>'
     venue = row[VENUE]
     presenter = assignments.get(row[TITLE], "TBD")
     table_row = f"<tr><td>{date}</td><td>{paper_link}</td><td>{presenter}</td><td>{venue}</td><td>{ta}</td></tr>\n"
