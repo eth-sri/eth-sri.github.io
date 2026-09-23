445ca2dee82d472145141287cbb41d2c8aa7d1ec
Niels <niels.muendler@inf.ethz.ch>
2026-09-23 17:25:52 +0200

    Publish student assignment SES2026 fall

--- _teaching/ses2026_assign.py
+++ _teaching/ses2026_assign.py
@@ -4,6 +4,7 @@
 #     "fire",
 #     "mip==1.16rc0",
 #     "cffi==2.0.0.b1",
+#     "edit_distance==1.0.9",
 # ]
 # ///
 import random
@@ -11,7 +12,28 @@ import csv
 from datetime import datetime
 import sys
 import fire
+import edit_distance
 random.seed(0)
+
+FIXED_ASSIGNMENT = {
+    "Computer science achievement and writing skills predict vibe coding proficiency": "Dario Llarden",
+    "Validating JVM Compilers via Maximizing Optimization Interactions": "Malte Dömer",
+}
+def row_of_student_name(
+    student_name: str,
+    enrollment: list[list[str]],
+):
+    min_dist = float("inf")
+    student = None
+    for row in enrollment:
+        student_name_official = f"{row[2]} {row[0]}"
+        dist_matcher = edit_distance.SequenceMatcher(a=student_name, b=student_name_official)
+        dist = dist_matcher.distance()
+        if dist < min_dist:
+            min_dist = dist
+            student = row
+    return student
+
 
 def solve_assignment_milp(
     paper_names: list[str],
@@ -29,13 +51,14 @@ def solve_assignment_milp(
     }
     from mip import Model, xsum, minimize, BINARY
     model = Model("MILP_01")
+    model.verbose = 0
     copied_pref_dict = {}
     for student in range(n_students):
         copied_pref_dict[student] = [model.add_var(var_type=BINARY) for _ in range(m_papers)]
     # Change to minimization problem.
     model.objective = minimize(
         xsum(
-            copied_pref_dict[student][paper] * (i + 1)**2
+            copied_pref_dict[student][paper] * (i + 1)**10
             for student, prefs in student_preferences.items()
             for i, paper in enumerate(prefs)
         )
@@ -56,28 +79,6 @@ def solve_assignment_milp(
             if copied_pref_dict[student][paper].x >= 0.9:
                 paper_assigned[paper_names[paper]] = students[student]
     return paper_assigned
-
-def simple_assignment(
-    paper_names: list[str],
-    students: list[str],
-    student_preferences: dict[str, list[str]],
-):
-    # assignment algorithm: stable marriage where students are randomized, non submitted preferences are randomized
-    paper_assigned = {
-        p: None for p in paper_names
-    }
-    students = list(students)
-    random.shuffle(students)
-    students_assigned = set()
-    for i in range(len(paper_assigned)):
-        for student in students:
-            if student not in students_assigned:
-                preferred_paper = student_preferences[student][i]
-                if not paper_assigned.get(preferred_paper):
-                    paper_assigned[preferred_paper] = student
-                    students_assigned.add(student)
-    return paper_assigned
-
 
 
 def assign_papers(
@@ -127,43 +128,51 @@ def assign_papers(
         submissions[row[3]] = row
 
 
-    paper_names = [p[1] for p in paper_rows]
+    paper_names = [p[1] for p in paper_rows if p[1] not in FIXED_ASSIGNMENT]
     students = list(students)
     student_preferences = {}
     for student, sub in submissions.items():
         prefs = []
         for i in range(4, 11):
-            if sub[i]:
+            if sub[i] in paper_names:
                 prefs.append(sub[i])
         non_submitted_prefs = [p for p in paper_names if p not in prefs]
         random.shuffle(non_submitted_prefs)
         prefs += non_submitted_prefs
         student_preferences[student] = prefs
 
-    # paper_assigned = simple_assignment(
-    #     paper_names=paper_names,
-    #     students=students,
-    #     student_preferences=student_preferences
-    # )
     paper_assigned = solve_assignment_milp(
         paper_names=paper_names,
         students=students,
         student_preferences=student_preferences
     )
 
-
     paper_by_name = {p[1]: p for p in paper_rows}
     student_by_legi = {s[3]: s for s in student_rows}
     writer = csv.writer(sys.stdout)
     writer.writerow(["Name", "Email", "Legi", "Ranked position", "Lab", "Paper title", "Paper link", "..."])
+    student_belegung_by_legi = {s[3]: s for s in enrollment}
     for paper in paper_rows:
         paper_name = paper[1]
-        if paper_assigned[paper_name] is None:
+        if paper_name in FIXED_ASSIGNMENT:
+            student_belegung_row = row_of_student_name(FIXED_ASSIGNMENT[paper_name], enrollment)
+            student_comments = ("Fixed as first lecture",)
+            is_among_preferences = True
+        elif paper_assigned[paper_name] is None:
             continue
-        student_details = student_by_legi[paper_assigned[paper_name]]
-        student_details = tuple(x.replace("\n", r"\n") for x in student_details)
-        is_among_preferences = student_preferences[student_details[3]].index(paper_name)
-        writer.writerow(student_details[1:4] + (is_among_preferences,) + tuple(paper_by_name[paper_name]) + student_details[11:13])
+        else:
+            legi = paper_assigned[paper_name]
+            student_details = student_by_legi[legi]
+            student_details = tuple(x.replace("\n", r"\n") for x in student_details)
+            student_comments = student_details[11:]
+            is_among_preferences = student_preferences[student_details[3]].index(paper_name)
+            student_belegung_row = student_belegung_by_legi[legi]
+        student_details = (
+            f"{student_belegung_row[2]} {student_belegung_row[0]}",
+            student_belegung_row[15],
+            student_belegung_row[3],
+        )
+        writer.writerow(student_details + (is_among_preferences,) + tuple(paper_by_name[paper_name]) + student_comments)
 
 if __name__ == "__main__":
     fire.Fire(assign_papers)
